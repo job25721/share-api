a74c364e9ff62ca7bd589165c7fa9139cc0ffd2f
job25721 <pathomporn_pankaew@cmu.ac.th>
2020-12-08 03:28:10 +0700

    rm dist

--- dist/ItemLog/logFunction.d.ts
+++
@@ -1,4 +0,0 @@
-import { Types } from 'mongoose';
-import { Log } from './dto/itemLog.model';
-export declare function calculateHash(data: any): string;
-export declare function createItemLog(actorId: Types.ObjectId | string, action: string): Log;
--- dist/User/user.resolver.d.ts
+++
@@ -1,8 +0,0 @@
-import { NewUser } from './dto/new-user';
-import { User } from './dto/user.model';
-import { UserService } from './user.service';
-export declare class UserResolver {
-    private readonly userService;
-    constructor(userService: UserService);
-    createUser(newUser: NewUser): Promise<User>;
-}
