3b5c4ac1557c832d7b9aea0c86ba723b3edd6164
job25721 <pathomporn_pankaew@cmu.ac.th>
2021-02-18 21:44:44 +0700

    rm rest user controller

--- src/User/user.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get, Post } from '@nestjs/common';
-
-@Controller('/api/user')
-export class UserController {
-  @Post('facebookSign')
-  facebookSign(): string {
-    return 'Hello';
-  }
-}
--- src/User/user.module.ts
+++ src/User/user.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserController } from './user.controller';
 import { UserResolver } from './user.resolver';
 import { UserSchema } from './user.schema';
 import { UserService } from './user.service';
@@ -9,6 +8,5 @@ import { UserService } from './user.service';
   imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
   providers: [UserResolver, UserService],
   exports: [UserService],
-  controllers: [UserController],
 })
 export class UserModule {}
